548e10627a6a8887993a399908f2f6703c4dd8e5
MarouaneDmy <marouane.demay@gmail.com>
2021-07-19 23:36:20 +0200

    Correction chemin data.json

--- javascript/DataManager.js
+++ javascript/DataManager.js
@@ -1,6 +1,6 @@
 export default class DataManager {
     async getData() {
-        let response = await fetch("../javascript/json/data.json")
+        let response = await fetch("/javascript/json/data.json")
         let data = await response.json()   
         return data
     }
